1094ec7e0442af6b05e591569f1d6a9838476ac0
shamodhasahan <sahanrathnamalala8@gmail.com>
2024-01-18 01:21:54 +0530

    add .env file

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
+import dotenv from "dotenv";
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import * as process from "process";
 
+dotenv.config();
 const port = 8080;
 const app = express();
 
 // @ts-ignore
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost:27017/movie_reservation");
+mongoose.connect(process.env.MONGO_URL as string);
 const db = mongoose.connection;
 
 db.on("error", (error) => {
