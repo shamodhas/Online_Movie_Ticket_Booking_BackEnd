8cc102ca76b2c986d480488b03c44dfa2f919b8c
shamodhasahan <sahanrathnamalala8@gmail.com>
2024-01-20 01:58:24 +0530

    add movie controller

--- src/controllers/movie.controller.ts
+++ src/controllers/movie.controller.ts
@@ -1,34 +1,39 @@
+import CustomResponse from "../dtos/custom.response";
 import express from "express";
 
-export const getAllMovies = async ( 
+export const getAllMovies = async (
   req: express.Request,
   res: express.Response
 ) => {};
 
-export const getMovieByName = async ( 
+export const getMovieByName = async (
   req: express.Request,
   res: express.Response
 ) => {};
 
-export const getMovieByUser = async ( 
+export const getMovieByUser = async (
   req: express.Request,
   res: express.Response
 ) => {};
 
-export const createMovie = async ( 
+export const createMovie = async (
   req: express.Request,
   res: express.Response
 ) => {
-    console.log('awa')
-    res.send('ok')
+  try {
+    
+  } catch (err) {
+    res.status(500).send(new CustomResponse(500, "Internal Server Error"));
+  }
 };
 
-export const updateMovie = async ( 
+export const updateMovie = async (
   req: express.Request,
   res: express.Response
 ) => {};
 
-export const deleteMovie = async ( // /{id}
+export const deleteMovie = async (
+  // /{id}
   req: express.Request,
   res: express.Response
 ) => {};
