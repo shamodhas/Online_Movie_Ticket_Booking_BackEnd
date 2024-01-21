43d1189cae0940167013ce327e9e457744178e93
shamodhasahan <sahanrathnamalala8@gmail.com>
2024-01-22 00:59:35 +0530

    modified:   src/controllers/hall.controller.ts

--- src/controllers/hall.controller.ts
+++ src/controllers/hall.controller.ts
@@ -1,30 +1,19 @@
+import express from "express";
 
+export const getAllHalls = (req: express.Request, res: express.Response) => {};
 
-export function getAllHalls(arg0: string, getAllHalls: any) {
-    throw new Error("Function not implemented.");
-}
+export const getHallByHallNumber = (
+  req: express.Request,
+  res: express.Response
+) => {};
 
+export const getHallsByUser = (
+  req: express.Request,
+  res: express.Response
+) => {};
 
-export function getHallByHallNumber(arg0: string, getHallByHallNumber: any) {
-    throw new Error("Function not implemented.");
-}
+export const saveHall = (req: express.Request, res: express.Response) => {};
 
+export const updateHall = (req: express.Request, res: express.Response) => {};
 
-export function getHallsByUser(arg0: string, getHallsByUser: any) {
-    throw new Error("Function not implemented.");
-}
-
-
-export function saveHall(arg0: string, verifyToken: (req: import("express").Request<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>, res: any, next: import("express").NextFunction) => Promise<any>, verifyIsTheaterEmployee: (req: import("express").Request<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>, res: any, next: import("express").NextFunction) => any, saveHall: any) {
-    throw new Error("Function not implemented.");
-}
-
-
-export function updateHall(arg0: string, verifyToken: (req: import("express").Request<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>, res: any, next: import("express").NextFunction) => Promise<any>, verifyIsTheaterEmployee: (req: import("express").Request<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>, res: any, next: import("express").NextFunction) => any, updateHall: any) {
-    throw new Error("Function not implemented.");
-}
-
-
-export function deleteHall(arg0: string, verifyToken: (req: import("express").Request<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>, res: any, next: import("express").NextFunction) => Promise<any>, verifyIsTheaterEmployee: (req: import("express").Request<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>, res: any, next: import("express").NextFunction) => any, deleteHall: any) {
-    throw new Error("Function not implemented.");
-}
+export const deleteHall = (req: express.Request, res: express.Response) => {};
