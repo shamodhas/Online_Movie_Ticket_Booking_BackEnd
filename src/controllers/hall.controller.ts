1676239880795a27945fc29802f26c6bd711e5b5
shamodhasahan <sahanrathnamalala8@gmail.com>
2024-01-22 23:20:43 +0530

    modified:   src/controllers/hall.controller.ts modified:   src/routes/hall.routes.ts

--- src/controllers/hall.controller.ts
+++ src/controllers/hall.controller.ts
@@ -1,4 +1,10 @@
 import express from "express";
+import HallModel from "../models/hall.model";
+import CustomResponse from "../dtos/custom.response";
+import * as SchemaTypes from "../types/SchemaTypes";
+import { ObjectId } from "mongodb";
+import UserModel from "../models/user.model";
+import * as RegexValidator from "../util/RegexValidator";
 
 export const getAllHalls = (req: express.Request, res: express.Response) => {};
 
@@ -7,12 +13,51 @@ export const getHallByHallNumber = (
   res: express.Response
 ) => {};
 
-export const getHallsByUser = (
-  req: express.Request,
-  res: express.Response
-) => {};
+export const getHallsByUser = (req: express.Request, res: express.Response) => {};
 
-export const saveHall = (req: express.Request, res: express.Response) => {};
+export const saveHall = (req: express.Request, res: any) => {
+  try {
+    const req_body: any = req.body;
+    const userId = res.tokenData.user._id;
+
+    if (!req_body.hallNumber) {
+      res.status(400).send(new CustomResponse(400, "Invalid hall number"));
+    }
+    // validate data
+    else {
+      let movie: SchemaTypes.IMovie | null = await MovieModel.findOne({
+        name: req_body.name,
+      });
+      if (movie) {
+        res.status(400).send(new CustomResponse(400, "Duplicate movie name"));
+      } else {
+        const movieModel = new MovieModel({
+          name: req_body.name,
+          director: req_body.director,
+          language: req_body.language,
+          description: req_body.description,
+          startDate: req_body.startDate,
+          endDate: req_body.endDate,
+          trailerLink: req_body.trailerLink,
+          status: "DEACTIVATE",
+          user: new ObjectId(userId),
+        });
+        await movieModel
+          .save()
+          .then((modelRes) => {
+            res
+              .status(200)
+              .send(new CustomResponse(200, "Movie saved", modelRes));
+          })
+          .catch((err) => {
+            res.status(500).send(new CustomResponse(500, "Fail to save movie"));
+          });
+      }
+    }
+  } catch (err) {
+    res.status(500).send(new CustomResponse(500, "Internal Server Error"));
+  }
+};
 
 export const updateHall = (req: express.Request, res: express.Response) => {};
 
--- src/routes/hall.routes.ts
+++ src/routes/hall.routes.ts
@@ -4,33 +4,33 @@ import * as HallController from "./../controllers/hall.controller";
 
 const router = express.Router();
 
-router.get("/all", HallController.getAllHalls);
+router.get("/all", HallController.getAllHalls);//
 
-router.get("/:hallNumber", HallController.getHallByHallNumber);
+router.get("/:hallNumber", HallController.getHallByHallNumber);//
 
-router.get("/user/:userId", HallController.getHallsByUser);
+router.get("/user/:userId", HallController.getHallsByUser);//
 
-router.get("/theater/:theaterId", HallController.getHallsByUser);
+router.get("/theater/:theaterId", HallController.getHallsByUser);//
 
 router.post(
   "/",
   Middleware.verifyToken,
   Middleware.verifyIsTheaterEmployee,
   HallController.saveHall
-);
+);//
 
 router.put(
   "/:id",
   Middleware.verifyToken,
   Middleware.verifyIsTheaterEmployee,
   HallController.updateHall
-);
+);//
 
 router.delete(
   "/:id",
   Middleware.verifyToken,
   Middleware.verifyIsTheaterEmployee,
   HallController.deleteHall
-);
+);//
 
 export default router;
