653365c147daaf03f5064f5d8cff8e7d9e8c5b95
shamodhas <shamodha7@gmail.com>
2025-09-18 04:37:10 +0530

    Changes to be committed:

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,14 +1,29 @@
+// firebase.ts
 import admin from "firebase-admin"
-import path from "path"
 
-const serviceAccountPath = path.join(__dirname, "../../serviceAccountKey.json")
+if (
+  !process.env.FIREBASE_TYPE ||
+  !process.env.FIREBASE_PROJECT_ID ||
+  !process.env.FIREBASE_PRIVATE_KEY ||
+  !process.env.FIREBASE_CLIENT_EMAIL ||
+  !process.env.FIREBASE_CLIENT_ID
+) {
+  throw new Error("Missing Firebase environment variables")
+}
 
+// Create service account object
+const serviceAccount: any = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID
+}
+
+// Initialize Firebase Admin
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccountPath),
-  storageBucket: "hyper-tech-425e4.firebasestorage.app"
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: "movie-booking-system-images.appspot.com" // your bucket
 })
 
-const bucket = admin.storage().bucket()
-
-export default bucket
-// gs://hyper-tech-425e4.firebasestorage.app/movie-booking-system-images
+export default admin
